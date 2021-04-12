790209d638b5688fd653859a309ffd868ba1682c
Cail Borg <cailborg@gmail.com>
2021-04-12 14:15:58 +1000

    Add files via upload

---
+++ build/precache-manifest.404ca9cafe6f81509540fe0730e0e123.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb8eb16956bfb0eef89766753b6a9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f19331dbc4ff1b2449",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2c6e4a9530a61c8b6793",
+    "url": "/static/css/main.ab098e70.chunk.css"
+  },
+  {
+    "revision": "61f19331dbc4ff1b2449",
+    "url": "/static/js/2.9692fbaa.chunk.js"
+  },
+  {
+    "revision": "2c6e4a9530a61c8b6793",
+    "url": "/static/js/main.1f80408f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa577c66583a673f208ed20b23e75277",
+    "url": "/static/media/GeographApp-Black.fa577c66.ttf"
+  },
+  {
+    "revision": "42e4cd25b34b57237e645b5a3901ffd1",
+    "url": "/static/media/GeographApp-BlackItalic.42e4cd25.ttf"
+  },
+  {
+    "revision": "4b07cdd289b6802af0222c619d0751e3",
+    "url": "/static/media/GeographApp-Bold.4b07cdd2.ttf"
+  },
+  {
+    "revision": "04d060e1aa534674c35023d4c02008d4",
+    "url": "/static/media/GeographApp-BoldItalic.04d060e1.ttf"
+  },
+  {
+    "revision": "a7ea79c8653fd221e98eef74b86ab63e",
+    "url": "/static/media/GeographApp-Light.a7ea79c8.ttf"
+  },
+  {
+    "revision": "add1902cb9ad019295b0cce62b3aaab8",
+    "url": "/static/media/GeographApp-LightItalic.add1902c.ttf"
+  },
+  {
+    "revision": "881464ff6abc0907704fecf5f56b28b8",
+    "url": "/static/media/GeographApp-Medium.881464ff.ttf"
+  },
+  {
+    "revision": "5d50e8174d5b46a90f9c49b5b1c7c8a7",
+    "url": "/static/media/GeographApp-MediumItalic.5d50e817.ttf"
+  },
+  {
+    "revision": "29064e3f0e4dd5c58bf9d3f00297eb95",
+    "url": "/static/media/GeographApp-Regular.29064e3f.ttf"
+  },
+  {
+    "revision": "08d4b5e7055bbdefa0c8392ecbfb10de",
+    "url": "/static/media/GeographApp-RegularItalic.08d4b5e7.ttf"
+  },
+  {
+    "revision": "5ee1148bc293f39aac24759a69b4b50a",
+    "url": "/static/media/GeographApp-Thin.5ee1148b.ttf"
+  },
+  {
+    "revision": "05985c8700f6301ed17de1a5660cf516",
+    "url": "/static/media/GeographApp-ThinItalic.05985c87.ttf"
+  },
+  {
+    "revision": "379a0a6e946aa44e9106641d73671306",
+    "url": "/static/media/geograph-web-black-italic.379a0a6e.woff"
+  },
+  {
+    "revision": "4ce5360a235e7273ad5839fece6d225b",
+    "url": "/static/media/geograph-web-black-italic.4ce5360a.eot"
+  },
+  {
+    "revision": "accb52254be7b41998bce7ed4216d450",
+    "url": "/static/media/geograph-web-black-italic.accb5225.woff2"
+  },
+  {
+    "revision": "83e7b5a671d5f03f13ebda6dfe6862e5",
+    "url": "/static/media/geograph-web-black.83e7b5a6.woff2"
+  },
+  {
+    "revision": "c79c3c169d5914a12fc4b6269c1da3e7",
+    "url": "/static/media/geograph-web-black.c79c3c16.eot"
+  },
+  {
+    "revision": "cf7ff4c369cd824012f5be24d7733b23",
+    "url": "/static/media/geograph-web-black.cf7ff4c3.woff"
+  },
+  {
+    "revision": "6944152e94bd8fb3bf104f4cb2171bc1",
+    "url": "/static/media/geograph-web-bold-italic.6944152e.eot"
+  },
+  {
+    "revision": "80c107766331f48548304750eb324467",
+    "url": "/static/media/geograph-web-bold-italic.80c10776.woff2"
+  },
+  {
+    "revision": "ed65de73aaaf5aa012fca9f4feb02830",
+    "url": "/static/media/geograph-web-bold-italic.ed65de73.woff"
+  },
+  {
+    "revision": "29109cea5deba9d585b7c7076209f1e1",
+    "url": "/static/media/geograph-web-bold.29109cea.eot"
+  },
+  {
+    "revision": "34b093a3e7fe3179d9d56ff8eb0a7bf9",
+    "url": "/static/media/geograph-web-bold.34b093a3.woff"
+  },
+  {
+    "revision": "dd7e9ef9cadef1f0d3a8fbff2cf8a246",
+    "url": "/static/media/geograph-web-bold.dd7e9ef9.woff2"
+  },
+  {
+    "revision": "7817dfaf343e3d9119109f41b32e67d4",
+    "url": "/static/media/geograph-web-light-italic.7817dfaf.woff2"
+  },
+  {
+    "revision": "a8a95edc1b8fb8709bdc9ecd36cbe6a1",
+    "url": "/static/media/geograph-web-light-italic.a8a95edc.woff"
+  },
+  {
+    "revision": "b295dd8a00f2dd6698a97e3a38360da8",
+    "url": "/static/media/geograph-web-light-italic.b295dd8a.eot"
+  },
+  {
+    "revision": "840e19810d2e9433cabb0797c43e28bc",
+    "url": "/static/media/geograph-web-light.840e1981.woff"
+  },
+  {
+    "revision": "9121a55a8a2a707ee3f1917d4759bf54",
+    "url": "/static/media/geograph-web-light.9121a55a.woff2"
+  },
+  {
+    "revision": "f47db94b5f88db3755a67046d4745120",
+    "url": "/static/media/geograph-web-light.f47db94b.eot"
+  },
+  {
+    "revision": "5d50dfcd414a9a3a4b9994c6122bf726",
+    "url": "/static/media/geograph-web-medium-italic.5d50dfcd.woff2"
+  },
+  {
+    "revision": "7160163b72b4df6b2c5c2dbaecf94a1b",
+    "url": "/static/media/geograph-web-medium-italic.7160163b.eot"
+  },
+  {
+    "revision": "78da104ccf8e5f470b631462f3d7c68a",
+    "url": "/static/media/geograph-web-medium-italic.78da104c.woff"
+  },
+  {
+    "revision": "49d4c86a33fb510e5b95bdf9fd27ca74",
+    "url": "/static/media/geograph-web-medium.49d4c86a.eot"
+  },
+  {
+    "revision": "76243c11b318084f2fdbe4a246243a12",
+    "url": "/static/media/geograph-web-medium.76243c11.woff"
+  },
+  {
+    "revision": "df280beb64520cfc43ecb25a982b426c",
+    "url": "/static/media/geograph-web-medium.df280beb.woff2"
+  },
+  {
+    "revision": "1548d725dcf0b11d356b372e26db10f8",
+    "url": "/static/media/geograph-web-regular-italic.1548d725.woff2"
+  },
+  {
+    "revision": "52555164f396adee168f86771da75269",
+    "url": "/static/media/geograph-web-regular-italic.52555164.eot"
+  },
+  {
+    "revision": "630af4fb390aaca82572cb4247d59c59",
+    "url": "/static/media/geograph-web-regular-italic.630af4fb.woff"
+  },
+  {
+    "revision": "2fd45b9ebd93f0cfa497ca0af3fe0d4d",
+    "url": "/static/media/geograph-web-regular.2fd45b9e.woff2"
+  },
+  {
+    "revision": "41af4a50d45e340cdd644fbc72f40108",
+    "url": "/static/media/geograph-web-regular.41af4a50.woff"
+  },
+  {
+    "revision": "fc70ad3697b064ae5dda0731ef3ce634",
+    "url": "/static/media/geograph-web-regular.fc70ad36.eot"
+  },
+  {
+    "revision": "56a7756435e0bbc07296d592f0f7889e",
+    "url": "/static/media/geograph-web-thin-italic.56a77564.woff"
+  },
+  {
+    "revision": "ab6828200e66588fb8622d0be587d024",
+    "url": "/static/media/geograph-web-thin-italic.ab682820.woff2"
+  },
+  {
+    "revision": "fdeb000cf18553029606696cc5f4f1dd",
+    "url": "/static/media/geograph-web-thin-italic.fdeb000c.eot"
+  },
+  {
+    "revision": "1cafe4e8b55c23e2834e6c2502dda73f",
+    "url": "/static/media/geograph-web-thin.1cafe4e8.woff2"
+  },
+  {
+    "revision": "6634f0a9c538712054e4c13ecc46cb21",
+    "url": "/static/media/geograph-web-thin.6634f0a9.woff"
+  },
+  {
+    "revision": "fc40540508c00249f1db1c9b38da7516",
+    "url": "/static/media/geograph-web-thin.fc405405.eot"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-stripe-menu/precache-manifest.74e1512e6dbc25ada479ed00b5474026.js"
+  "/precache-manifest.404ca9cafe6f81509540fe0730e0e123.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-stripe-menu/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
